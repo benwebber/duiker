e89624642a3d9847fd0de5a21e66fb3e4b6a1298
Ben Webber <benjamin.webber@gmail.com>
2017-05-02 04:14:00 +0000

    Add tests for parse_history_line()

--- test_duiker.py
+++ test_duiker.py
@@ -1,3 +1,6 @@
+from datetime import datetime as dt
+import time
+
 import pytest
 
 import duiker
@@ -34,3 +37,36 @@ def test_sizeof_human_binary(size, expected):
 ])
 def test_sizeof_human_decimal(size, expected):
     assert duiker.sizeof_human(size, False) == expected
+
+
+@pytest.fixture
+def millenium():
+    from datetime import datetime as dt
+    return dt.strptime('2001-01-01 00:00:00', '%Y-%m-%d %H:%M:%S')
+
+
+@pytest.fixture
+def millenium_unix():
+    import time
+    return time.mktime(millenium().timetuple())
+
+
+@pytest.mark.parametrize('line,histtimeformat,expected', [
+    ('  1  help history', None, duiker.Command(None, None, 'help history')),
+    ('100  help history', None, duiker.Command(None, None, 'help history')),
+    ('  1  2001-01-01 00:00:00 help history', '%Y-%m-%d %H:%M:%S ', duiker.Command(None, millenium_unix(), 'help history')),
+    ('100  2001-01-01 00:00:00 help history', '%Y-%m-%d %H:%M:%S ', duiker.Command(None, millenium_unix(), 'help history')),
+    ('  1  2001-01-01 00:00:00 help history', '%Y-%m-%d %H:%M:%S', duiker.Command(None, millenium_unix(), 'help history')),
+    ('100  2001-01-01 00:00:00 help history', '%Y-%m-%d %H:%M:%S', duiker.Command(None, millenium_unix(), 'help history')),
+])
+def test_parse_history_line(line, histtimeformat, expected):
+    assert duiker.parse_history_line(line, histtimeformat) == expected
+
+
+@pytest.mark.parametrize('line,histtimeformat', [
+    ('  1  2001-01-01 00:00:00 help history', '%%'),
+    ('100  2001-01-01 00:00:00 help history', '%%'),
+])
+def test_parse_history_line_failure(line, histtimeformat):
+    with pytest.raises(ValueError):
+        duiker.parse_history_line(line, histtimeformat)

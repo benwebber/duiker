f508b1610617322ee9c6bc3e4c5dbd02228e9ac5
MattK <matthew.keranen@highwinds.com>
2017-05-13 18:04:03 -0400

    Replace tilde with expanduser()

--- src/duiker.py
+++ src/duiker.py
@@ -36,7 +36,7 @@ def xdg_data_home(name: Optional[str] = None) -> pathlib.Path:
     Return the XDG Base Directory Specification data directory for a specific
     application, or the base directory itself.
     """
-    home = pathlib.Path(os.path.expanduser(os.environ.get('XDG_DATA_HOME') or '~/.local/share'))
+    home = pathlib.Path(os.path.expanduser(os.environ.get('XDG_DATA_HOME') or os.path.expanduser('~') + '/.local/share'))
     return home / name if name else home
 
 

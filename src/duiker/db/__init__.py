d0d55b9c59de70453a8726a8a3f41b65bb26fcdc
Ben Webber <benjamin.webber@gmail.com>
2017-05-20 17:29:16 +0000

    Wrap database connection as DAO

--- src/duiker/db/__init__.py
+++ src/duiker/db/__init__.py
@@ -19,6 +19,26 @@ from ..types import (
 from ..parser import Command
 
 
+class DAO:
+    def __init__(self, database):
+        self.database = str(database)
+
+    @property
+    def connection(self):
+        return sqlite3.connect(self.database)
+
+    @property
+    def user_version(self):
+        with self.connection as connection:
+            return connection.execute('PRAGMA user_version').fetchone()[0]
+
+    @user_version.setter
+    def user_version(self, version):
+        with self.connection as connection:
+            connection.execute('PRAGMA user_version = {}'.format(version))
+
+
+
 class MigrationError(Exception):
     pass
 
@@ -40,64 +60,50 @@ class MigrationGraph:
 
 class Migrator:
     def __init__(self, database, create_migrations_table=True):
-        self.database = database
+        self.database = DAO(database)
 
         if create_migrations_table:
-            with self.connection as connection:
+            with self.database.connection as connection:
                 connection.execute('CREATE TABLE IF NOT EXISTS migrations (name TEXT NOT NULL UNIQUE)')
 
     @property
     def applied_migrations(self):
         return list(self.iter_applied_migrations())
 
-    @property
-    def connection(self):
-        return sqlite3.connect(self.database)
-
-    @property
-    def user_version(self):
-        with self.connection as connection:
-            return connection.execute('PRAGMA user_version').fetchone()[0]
-
-    @user_version.setter
-    def user_version(self, version):
-        with self.connection as connection:
-            connection.execute('PRAGMA user_version = {}'.format(version))
-
     def _delete(self, migration):
         name = get_migration_name(migration)
-        with self.connection as connection:
+        with self.database.connection as connection:
             connection.execute('DELETE FROM migrations WHERE name = ?', (name,))
 
     def _insert(self, migration):
         name = get_migration_name(migration)
-        with self.connection as connection:
+        with self.database.connection as connection:
             connection.execute('INSERT INTO migrations VALUES (?)', (name,))
 
     def apply(self, migration):
-        with self.connection as connection:
+        with self.database.connection as connection:
             try:
                 migration.apply(connection)
             except AttributeError:
                 raise MigrationError('Migrations must define a forward operation (`apply()`).')
         self._insert(migration)
         if getattr(migration, '__bump_version__', False):
-            self.user_version += 1
+            self.database.user_version += 1
 
     def iter_applied_migrations(self):
-        with self.connection as connection:
+        with self.database.connection as connection:
             for row in connection.execute('SELECT name FROM migrations'):
                 yield row[0]
 
     def rollback(self, migration):
-        with self.connection as connection:
+        with self.database.connection as connection:
             try:
                 migration.rollback(connection)
             except AttributeError:
                 raise MigrationError('Migration {} cannot be reverted.'.format(get_migration_name(migration)))
         self._delete(migration)
         if getattr(migration, '__bump_version__', False):
-            self.user_version -= 1
+            self.database.user_version -= 1
 
 
 def get_migration_name(module):
